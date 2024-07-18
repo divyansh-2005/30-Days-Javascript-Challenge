3d506edc931633efff442a16108b9c7d02a734cf
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-07-18 17:33:24 +0530

    day 1

---
+++ Day1.js
@@ -0,0 +1,40 @@
+//VARIABLES AND DATA TYPES
+
+//DAY 1
+
+//1 variable Declaration
+var num = 10;
+console.log(num);
+
+let str = "DAY 1";
+console.log(str);
+
+//2 Constant Declaration
+const bool = true;
+console.log(bool);
+
+//3 Data Types
+const num1 = 10;
+const str1 = "DAY 1";
+const bool1 = false;
+const obj = {1:"first",2:"second"};
+const arr = [1,2,3,4,5];
+
+console.log(typeof(num1));
+console.log(typeof(str1));
+console.log(typeof(bool1));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+//4 Reassigning Variables
+let var1 = 10;
+console.log(var1);
+var1 = 11;
+console.log(var1);
+
+//5 Understanding const
+const var2 = 10;
+console.log(var2);
+var2 = 11;
+console.log(var2);
+
